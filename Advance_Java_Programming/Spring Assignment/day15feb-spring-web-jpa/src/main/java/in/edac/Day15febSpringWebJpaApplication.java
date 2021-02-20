8767c6b5250d4b5dcf4012ac9c830a2469e238a5
pratimajadhav2312 <72022208+pratimajadhav2312@users.noreply.github.com>
2021-02-20 14:27:15 +0530

    Add files via upload

---
+++ Advance_Java_Programming/Spring Assignment/day15feb-spring-web-jpa/src/main/java/in/edac/Day15febSpringWebJpaApplication.java
@@ -0,0 +1,13 @@
+package in.edac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day15febSpringWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day15febSpringWebJpaApplication.class, args);
+	}
+
+}
